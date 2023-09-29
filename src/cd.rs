3c6192ef4f00d5103596fdcbe80cc9120d8eaf84
Chris Ha <hac541309@gmail.com>
2023-09-29 12:13:16 +0900

    remove wild card import
    wild card imports can cause unintended consequences

--- src/cd.rs
+++ src/cd.rs
@@ -1,8 +1,11 @@
 #![allow(unused_variables)]
-use crate::assets::*;
+use crate::assets::{ENCODING_TO_LANGUAGE, LANGUAGES};
 use crate::consts::TOO_SMALL_SEQUENCE;
-use crate::entity::*;
-use crate::utils::*;
+use crate::entity::{CoherenceMatch, CoherenceMatches, Language};
+use crate::utils::{
+    get_language_data, is_accentuated, is_multi_byte_encoding, is_suspiciously_successive_range,
+    is_unicode_range_secondary, unicode_range,
+};
 use ahash::{HashMap, HashMapExt, HashSet};
 use cached::proc_macro::cached;
 use counter::Counter;
