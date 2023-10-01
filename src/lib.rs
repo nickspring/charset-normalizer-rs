08c4981dddf9a24e1c2c3cfbdc7a39fce6d1cc15
chris-ha458 <hac541309@gmail.com>
2023-10-01 14:50:16 +0900

    change to match

--- src/lib.rs
+++ src/lib.rs
@@ -572,26 +572,26 @@ pub fn from_bytes(bytes: &[u8], settings: Option<NormalizerSettings>) -> Charset
 
     // fallbacks
     if results.is_empty() {
-        let mut fb: Option<&CharsetMatch> = None;
-        if fallback_specified.is_some() {
-            fb = Some(fallback_specified.as_ref().unwrap());
-        } else if fallback_u8.is_some()
-            && (fallback_ascii.is_none()
-                || (fallback_ascii.is_some()
-                    && fallback_u8.as_ref().unwrap().decoded_payload()
-                        != fallback_ascii.as_ref().unwrap().decoded_payload()))
-        {
-            fb = Some(fallback_u8.as_ref().unwrap());
-        } else if fallback_ascii.is_some() {
-            fb = Some(fallback_ascii.as_ref().unwrap());
-        }
-        if let Some(fb_to_pass) = fb {
+        let fb = match (&fallback_specified, &fallback_u8, &fallback_ascii) {
+            (Some(specified), _, _) => Some(specified),
+            (None, Some(u8_fallback), None) => Some(u8_fallback),
+            (None, Some(u8_fallback), Some(ascii)) => {
+                if u8_fallback.decoded_payload() != ascii.decoded_payload() {
+                    Some(u8_fallback)
+                } else {
+                    Some(ascii)
+                }
+            }
+            (None, _, Some(ascii)) => Some(ascii),
+            _ => None,
+        };
+        fb.map(|fb_to_pass| {
             debug!(
                 "Encoding detection: will be used as a fallback match {}",
                 fb_to_pass.encoding()
             );
             results.append(fb_to_pass.clone());
-        }
+        });
     }
 
     // final logger information
