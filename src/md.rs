bd019e8e312a56b3eb18685c9c12f6bece3554e6
chris-ha458 <hac541309@gmail.com>
2023-10-04 20:48:09 +0900

    cargo fmt

--- src/md.rs
+++ src/md.rs
@@ -51,9 +51,10 @@ impl MessDetectorPlugin for TooManySymbolOrPunctuationPlugin {
     }
     fn feed(&mut self, character: char) {
         self.character_count += 1;
-        let is_different_char = self.last_printable_char.map_or(true, |last_char| character != last_char);
-        if is_different_char && !COMMON_SAFE_ASCII_CHARACTERS.contains(character)
-        {
+        let is_different_char = self
+            .last_printable_char
+            .map_or(true, |last_char| character != last_char);
+        if is_different_char && !COMMON_SAFE_ASCII_CHARACTERS.contains(character) {
             if is_punctuation(character) {
                 self.punctuation_count += 1;
             } else if !character.is_numeric() && is_symbol(character) && !is_emoticon(character) {
